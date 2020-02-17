9fff52863c57fb5b294c8e222571375b9eef3a06
Alex <sasha.karpenkov@outlook.com>
2020-02-17 19:21:24 +0300

    init socket.io

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+let count = 0
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New websocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
